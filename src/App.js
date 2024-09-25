8670220c0b0d88519e7748d2ffdebc895ed82253
Patrick Arganza <arganzapatrick@gmail.com>
2024-09-25 16:08:30 +0800

    Cookie Added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Stats from "./components/Stats";
 import Why from "./components/Why";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
-import Faq from "./components/faq"; // Make sure 'Faq' matches your file's case
+import Faq from "./components/faq";
 import News from "./components/News";
 import Company from "./components/Company";
+import Cookie from "./components/Cookie";
 
 import { useEffect } from "react";
 import "aos/dist/aos.css";
@@ -16,12 +17,11 @@ import AOS from "aos";
 function App() {
   useEffect(() => {
     AOS.init({
-      duration: 1200, // Set animation duration (optional)
-      once: true, // Animation will occur only once when scrolling down (optional)
+      duration: 1200,
+      once: true,
     });
   }, []);
 
-  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   if (
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
@@ -36,6 +36,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <Cookie />
       <Services />
       <Stats />
       <Why />
@@ -43,6 +44,7 @@ function App() {
       <Testimonials />
       <Faq />
       <News />
+
       <Footer />
     </div>
   );
--- src/components/misc/news_card.jsx
+++ src/components/misc/news_card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Toast from "../Toast";
 
 const News_Card = () => {
   return (
